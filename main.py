cb51e820e351e8242ba7e585fabcabd983421f5a
Azim Sofi <89006182+AzimSofi@users.noreply.github.com>
2023-04-28 20:47:22 +0800

    Added spaceship

--- main.py
+++ main.py
@@ -2,20 +2,47 @@
 # https://www.youtube.com/watch?v=jO6qQDNa2UY&ab_channel=TechWithTim
 
 import pygame
+import os # to import path
 
 WIDTH,HEIGHT = 800,600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("初めて")
+
+RAINBOW_INDIGO = (30, 63, 102)
+FPS  = 144
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55,40
+
+# Object to be drawn (surface)
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+     os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(
+     YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)), 90) # Rotate the resized spaceship
+
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+RED_SPACESHIP_IMAGE = pygame.transform.rotate(pygame.transform.scale(
+     RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH,SPACESHIP_HEIGHT)), -90) # Rotate the resized spaceship
+
+def draw_window():
+        # Draw background before surface
+        WIN.fill(RAINBOW_INDIGO)
+        # To draw surface
+        # D
+        WIN.blit(YELLOW_SPACESHIP_IMAGE, (50,280)) # Within dimenssion of Window's W,H
+        WIN.blit(RED_SPACESHIP_IMAGE, (700,280)) # Within dimenssion of Window's W,H
+        pygame.display.update() # update the "drawing"
 
 # redrawing window, checking collision, update score など
 def main():
-    
+    clock = pygame.time.Clock()
     # Event loop
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             # quit
             if event.type == pygame.QUIT: # pygame.QUIT -> x button top-right
                 run = False
+        draw_window()
     
     pygame.quit()
 
